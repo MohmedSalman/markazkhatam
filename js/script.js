89fa1a2fb6da00dbaec46a385e8ae2233c67bebd
Mohmed Salman <77356995+MohmedSalman@users.noreply.github.com>
2021-09-05 04:51:34 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,647 @@
+
+
+         
+alrt = document.getElementById("alert");
+
+
+//const gravarB = document.getElementById("gravar");
+const rst = document.getElementById("reset");
+
+
+/*const itm1 = document.getElementById("item1");
+const itm2 = document.getElementById("item2");
+const itm3 = document.getElementById("item3");
+const itm4 = document.getElementById("item4");
+const itm5 = document.getElementById("item5");
+const itm6 = document.getElementById("item6");
+const itm7 = document.getElementById("item7");
+const itm8 = document.getElementById("item8");
+const itm9 = document.getElementById("item9");
+const itm10 = document.getElementById("item10");
+const itm11 = document.getElementById("item11");
+const itm12 = document.getElementById("item12");
+const itm13 = document.getElementById("item13");
+const itm14 = document.getElementById("item14");
+const itm15 = document.getElementById("item15");
+const itm16 = document.getElementById("item16");
+const itm17 = document.getElementById("item17");
+const itm18 = document.getElementById("item18");
+const itm19 = document.getElementById("item19");
+const itm20 = document.getElementById("item20");
+const itm21 = document.getElementById("item21");
+const itm22 = document.getElementById("item22");
+const itm23 = document.getElementById("item23");
+const itm24 = document.getElementById("item24");
+const itm25 = document.getElementById("item25");
+const itm26 = document.getElementById("item26");
+const itm27 = document.getElementById("item27");
+const itm28 = document.getElementById("item28");
+const itm29 = document.getElementById("item29");
+const itm30 = document.getElementById("item30");
+
+
+
+
+
+gravarB.onclick = function(){
+
+	  
+
+
+	    const num = "1";
+		const key = itm1.value;
+
+
+		const num2 = "2";
+		const key2 = itm2.value;
+
+		const num3 = "3";
+		const key3 = itm3.value;
+
+		const num4 = "4";
+		const key4 = itm4.value;
+
+		const num5 = "5";
+		const key5 = itm5.value;
+
+		const num6 = "6";
+		const key6 = itm6.value;
+
+		const num7 = "7";
+		const key7 = itm7.value;
+
+		const num8 = "8";
+		const key8 = itm8.value;
+
+		const num9 = "9";
+		const key9 = itm9.value;
+
+		const num10 = "10";
+		const key10 = itm10.value;
+
+		const num11 = "11";
+		const key11 = itm11.value;
+
+		const num12 = "12";
+		const key12 = itm12.value;
+
+		const num13 = "13";
+		const key13 = itm13.value;
+
+		const num14 = "14";
+		const key14 = itm14.value;
+
+		const num15 = "15";
+		const key15 = itm15.value;
+
+		const num16 = "16";
+		const key16 = itm16.value;
+
+		const num17 = "17";
+		const key17 = itm17.value;
+
+		const num18 = "18";
+		const key18 = itm18.value;
+
+		const num19 = "19";
+		const key19 = itm19.value;
+
+		const num20 = "20";
+		const key20 = itm20.value;
+
+		const num21 = "21";
+		const key21 = itm21.value;
+
+		const num22 = "22";
+		const key22 = itm22.value;
+
+		const num23 = "23";
+		const key23 = itm23.value;
+
+		const num24 = "24";
+		const key24 = itm24.value;
+
+		const num25 = "25";
+		const key25 = itm25.value;
+
+		const num26 = "26";
+		const key26 = itm26.value;
+
+		const num27 = "27";
+		const key27 = itm27.value;
+
+		const num28 = "28";
+		const key28 = itm28.value;
+		
+		const num29 = "29";
+		const key29 = itm29.value;
+
+		const num30 = "30";
+		const key30 = itm30.value;
+
+		//const key1 = inpKey1.value;
+		//const value1 = inpValue1.value;
+
+		//console.log(key);
+		//console.log(num);
+		
+		
+       
+		if(key)
+		{
+			localStorage.setItem(num, key);
+			location.reload();
+		}
+
+		if(key2)
+		{
+			localStorage.setItem(num2, key2);
+			location.reload();
+		}
+
+		if(key3)
+		{
+			localStorage.setItem(num3, key3);
+			location.reload();
+		}
+		if(key4)
+		{
+			localStorage.setItem(num4, key4);
+			location.reload();
+		}
+		if(key5)
+		{
+			localStorage.setItem(num5, key5);
+			location.reload();
+		}
+        if(key6)
+		{
+			localStorage.setItem(num6, key6);
+			location.reload();
+		}
+         if(key7)
+		{
+			localStorage.setItem(num7, key7);
+			location.reload();
+		}
+		if(key8)
+		{
+			localStorage.setItem(num8, key8);
+			location.reload();
+		}
+
+       if(key9)
+		{
+			localStorage.setItem(num9, key9);
+			location.reload();
+		}
+        
+        if(key10)
+		{
+			localStorage.setItem(num10, key10);
+			location.reload();
+		}
+
+		 if(key11)
+		{
+			localStorage.setItem(num11, key11);
+			location.reload();
+		}
+
+		 if(key12)
+		{
+			localStorage.setItem(num12, key12);
+			location.reload();
+		}
+
+
+		 if(key13)
+		{
+			localStorage.setItem(num13, key13);
+			location.reload();
+		}
+
+		 if(key14)
+		{
+			localStorage.setItem(num14, key14);
+			location.reload();
+		}
+      
+		if(key15)
+		{
+			localStorage.setItem(num15, key15);
+			location.reload();
+		}
+
+		if(key16)
+		{
+			localStorage.setItem(num16, key16);
+			location.reload();
+		}
+
+		if(key17)
+		{
+			localStorage.setItem(num17, key17);
+			location.reload();
+		}
+
+		if(key18)
+		{
+			localStorage.setItem(num18, key18);
+			location.reload();
+		}
+
+		if(key19)
+		{
+			localStorage.setItem(num19, key19);
+			location.reload();
+		}
+
+		if(key20)
+		{
+			localStorage.setItem(num20, key20);
+			location.reload();
+		}
+
+		if(key21)
+		{
+			localStorage.setItem(num21, key21);
+			location.reload();
+		}
+
+		if(key22)
+		{
+			localStorage.setItem(num22, key22);
+			location.reload();
+		}
+
+		if(key23)
+		{
+			localStorage.setItem(num23, key23);
+			location.reload();
+		}
+        
+        if(key24)
+		{
+			localStorage.setItem(num24, key24);
+			location.reload();
+		}
+
+		 if(key25)
+		{
+			localStorage.setItem(num25, key25);
+			location.reload();
+		}
+
+		if(key26)
+		{
+			localStorage.setItem(num26, key26);
+			location.reload();
+		}
+
+        if(key27)
+		{
+			localStorage.setItem(num27, key27);
+			location.reload();
+		}
+
+		 if(key28)
+		{
+			localStorage.setItem(num28, key28);
+			location.reload();
+		}
+
+		if(key29)
+		{
+			localStorage.setItem(num29, key29);
+			location.reload();
+		}
+
+
+		if(key30)
+		{
+			localStorage.setItem(num30, key30);
+			location.reload();
+		}
+
+	};
+		
+
+
+	for(let i = 0; i < localStorage.length;i++)
+	{
+		const key =localStorage.key(i);
+		const value = localStorage.getItem(key);
+		var op = localStorage.getItem("1");
+		itm1.value = op;
+		
+	}
+
+	for(let j = 0; j < localStorage.length;j++)
+	{
+		const key2 =localStorage.key(j);
+		const val2 = localStorage.getItem(key2);
+		var op2 = localStorage.getItem("2");
+		itm2.value = op2;
+		
+	}
+
+    for(let j = 0; j < localStorage.length;j++)
+	{
+		const key3 =localStorage.key(j);
+		const val3 = localStorage.getItem(key3);
+		var op3 = localStorage.getItem("3");
+		itm3.value = op3;
+        isOutput.innerHTML = localStorage.length;
+		
+	}
+
+   for(let j = 0; j < localStorage.length;j++)
+	{
+		const key4 =localStorage.key(j);
+		const val4 = localStorage.getItem(key4);
+		var op4 = localStorage.getItem("4");
+		itm4.value = op4;
+		
+	}
+
+	  for(let j = 0; j < localStorage.length;j++)
+	{
+		const key5 =localStorage.key(j);
+		const val5 = localStorage.getItem(key5);
+		var op5 = localStorage.getItem("5");
+		itm5.value = op5;
+		
+	}
+
+	 for(let j = 0; j < localStorage.length;j++)
+	{
+		const key6 =localStorage.key(j);
+		const val6 = localStorage.getItem(key6);
+		var op6 = localStorage.getItem("6");
+		itm6.value = op6;
+	}
+
+	 for(let j = 0; j < localStorage.length;j++)
+	{
+		const key7 =localStorage.key(j);
+		const val7 = localStorage.getItem(key7);
+		var op7 = localStorage.getItem("7");
+		itm7.value = op7;
+	}
+
+	 for(let j = 0; j < localStorage.length;j++)
+	{
+		const key8 =localStorage.key(j);
+		const val8 = localStorage.getItem(key8);
+		var op8 = localStorage.getItem("8");
+		itm8.value = op8;
+	}
+
+	 for(let j = 0; j < localStorage.length;j++)
+	{
+		const key9 =localStorage.key(j);
+		const val9 = localStorage.getItem(key9);
+		var op9 = localStorage.getItem("9");
+		itm9.value = op9;
+	}
+
+	for(let j = 0; j < localStorage.length;j++)
+	{
+		const key10 =localStorage.key(j);
+		const val10 = localStorage.getItem(key10);
+		var op10 = localStorage.getItem("10");
+		itm10.value = op10;
+	}
+
+	for(let j = 0; j < localStorage.length;j++)
+	{
+		const key11 =localStorage.key(j);
+		const val11 = localStorage.getItem(key11);
+		var op11 = localStorage.getItem("11");
+		itm11.value = op11;
+	}
+
+	for(let j = 0; j < localStorage.length;j++)
+	{
+		const key12 =localStorage.key(j);
+		const val12 = localStorage.getItem(key12);
+		var op12 = localStorage.getItem("12");
+		itm12.value = op12;
+	}
+     
+    for(let j = 0; j < localStorage.length;j++)
+	{
+		const key13 =localStorage.key(j);
+		const val13 = localStorage.getItem(key13);
+		var op13 = localStorage.getItem("13");
+		itm13.value = op13;
+	}
+    
+     for(let j = 0; j < localStorage.length;j++)
+	{
+		const key14 =localStorage.key(j);
+		const val14 = localStorage.getItem(key14);
+		var op14 = localStorage.getItem("14");
+		itm14.value = op14;
+	}
+
+	 for(let j = 0; j < localStorage.length;j++)
+	{
+		const key15 =localStorage.key(j);
+		const val15 = localStorage.getItem(key15);
+		var op15 = localStorage.getItem("15");
+		itm15.value = op15;
+		
+	}
+
+	 for(let j = 0; j < localStorage.length;j++)
+	{
+		const key16 =localStorage.key(j);
+		const val16 = localStorage.getItem(key16);
+		var op16 = localStorage.getItem("16");
+		itm16.value = op16;
+		
+	}
+
+	 for(let j = 0; j < localStorage.length;j++)
+	{
+		const key17 =localStorage.key(j);
+		const val17 = localStorage.getItem(key17);
+		var op17 = localStorage.getItem("17");
+		itm17.value = op17;
+		
+	}
+
+	for(let j = 0; j < localStorage.length;j++)
+	{
+		const key18 =localStorage.key(j);
+		const val18 = localStorage.getItem(key18);
+		var op18 = localStorage.getItem("18");
+		itm18.value = op18;
+		
+	}
+
+	for(let j = 0; j < localStorage.length;j++)
+	{
+		const key19 =localStorage.key(j);
+		const val19 = localStorage.getItem(key19);
+		var op19 = localStorage.getItem("19");
+		itm19.value = op19;
+		
+	}
+	for(let j = 0; j < localStorage.length;j++)
+	{
+		const key20 =localStorage.key(j);
+		const val20 = localStorage.getItem(key20);
+		var op20 = localStorage.getItem("20");
+		itm20.value = op20;
+	}
+
+	for(let j = 0; j < localStorage.length;j++)
+	{
+		const key21 =localStorage.key(j);
+		const val21 = localStorage.getItem(key21);
+		var op21 = localStorage.getItem("21");
+		itm21.value = op21;
+	}
+	for(let j = 0; j < localStorage.length;j++)
+	{
+		const key22 =localStorage.key(j);
+		const val22 = localStorage.getItem(key22);
+		var op22 = localStorage.getItem("22");
+		itm22.value = op22;
+	}
+
+	for(let j = 0; j < localStorage.length;j++)
+	{
+		const key23 =localStorage.key(j);
+		const val23 = localStorage.getItem(key23);
+		var op23 = localStorage.getItem("23");
+		itm23.value = op23;
+	}
+
+	for(let j = 0; j < localStorage.length;j++)
+	{
+		const key24 =localStorage.key(j);
+		const val24 = localStorage.getItem(key24);
+		var op24 = localStorage.getItem("24");
+		itm24.value = op24;
+	}
+
+	for(let j = 0; j < localStorage.length;j++)
+	{
+		const key25 =localStorage.key(j);
+		const val25 = localStorage.getItem(key25);
+		var op25 = localStorage.getItem("25");
+		itm25.value = op25;
+	}
+
+	for(let j = 0; j < localStorage.length;j++)
+	{
+		const key26 =localStorage.key(j);
+		const val26 = localStorage.getItem(key26);
+		var op26 = localStorage.getItem("26");
+		itm26.value = op26;
+	}
+
+	for(let j = 0; j < localStorage.length;j++)
+	{
+		const key27 =localStorage.key(j);
+		const val27 = localStorage.getItem(key27);
+		var op27 = localStorage.getItem("27");
+		itm27.value = op27;
+	}
+
+	for(let j = 0; j < localStorage.length;j++)
+	{
+		const key28 =localStorage.key(j);
+		const val28 = localStorage.getItem(key28);
+		var op28 = localStorage.getItem("28");
+		itm28.value = op28;
+	}
+
+	for(let j = 0; j < localStorage.length;j++)
+	{
+		const key29 =localStorage.key(j);
+		const val29 = localStorage.getItem(key29);
+		var op29 = localStorage.getItem("29");
+		itm29.value = op29;
+	}
+
+
+
+
+
+	for(let j = 0; j < localStorage.length;j++)
+	{
+		const key30 =localStorage.key(j);
+		const value = localStorage.getItem(key30);
+		var op30 = localStorage.getItem("30");
+		itm30.value = op30;
+		
+	}*/
+
+
+
+
+
+
+    
+function handleScrollEvents()
+{
+	handleStickyNav(),handleElementsTransitions()
+}
+function handleResponsiveNav()
+{
+	document.querySelector(".menu-container").classList.toggle("change"),document.querySelector(".main-nav-links").classList.toggle("show")
+}
+
+function handleStickyNav()
+{
+	window.scrollY>=100&&!stickyEnabled&&document.querySelector(".main-nav").classList.add("hide-nav"),window.scrollY<100&&(!stickyEnabled||stickyEnabled)&&document.querySelector(".main-nav").classList.remove("hide-nav"),window.scrollY>200&&!stickyEnabled&&(document.querySelector(".main-nav").classList.add("sticky-nav"),stickyEnabled=!0),window.scrollY<=200&&stickyEnabled&&(document.querySelector(".main-nav").classList.remove("sticky-nav"),stickyEnabled=!1),window.scrollY>=460&&!stickyShown&&(document.querySelector(".main-nav").classList.add("show-sticky"),stickyShown=!0),window.scrollY<460&&stickyShown&&(document.querySelector(".main-nav").classList.remove("show-sticky"),stickyShown=!1)
+}
+function handleElementsTransitions()
+{
+	width>=1024?window.scrollY>470&&document.querySelector(".notes-section .description").classList.add("show-up"):window.scrollY>700&&document.querySelector(".notes-section .description").classList.add("show-up"),width<=768&&(window.scrollY>320&&!notesAppShown&&(document.querySelector(".notes-app").classList.add("slide-in"),notesAppShown=!0),window.scrollY>420&&!todoAppShown&&(document.querySelector(".todo-app").classList.add("slide-up"),todoAppShown=!0))}function handleLetsGoUpExit(){goUpShown&&(document.querySelector(".lets-go-up").classList.add("hide-lets-go-up"),document.querySelector(".lets-go-up").classList.remove("show-lets-go-up"),document.querySelector(".lets-go-up").onanimationend=(()=>{document.querySelector(".lets-go-up").classList.remove("hide-lets-go-up")}),goUpShown=!1)
+}
+function handleLetsGoUpEntrance()
+{
+	document.querySelector(".lets-go-up").classList.add("show-lets-go-up"),goUpShown=!0
+}
+function handleTakeATourClick()
+{
+	const e=document.querySelector(".notes-app").offsetTop-60;window.scrollTo(0,e,"smooth")
+}
+function handleGoUpClick()
+{
+	const e=document.querySelector("header").offsetTop;window.scrollTo(0,e,"smooth")
+}
+function handleLogoClick()
+{
+	window.scroll(0,0,"smooth");const e=document.querySelector(".show");e&&handleResponsiveNav()
+}
+
+function handleResponsiveMenuLinkClick(e)
+{
+	const o=document.querySelector(".show");if(o){const o=Array.from(e.target.classList).join(" ");o.includes("js-normal_link")&&handleResponsiveNav()
+}}
+
+let stickyEnabled=!1,stickyShown=!1,notesAppShown=!1,todoAppShown=!1,goUpShown=!1;const width=document.body.offsetWidth;document.querySelector(".go-up").addEventListener("mouseover",handleLetsGoUpEntrance),document.querySelector(".go-up").addEventListener("mouseout",handleLetsGoUpExit),document.querySelector(".logo").addEventListener("click",handleLogoClick),document.querySelector(".main-nav-links").onclick=handleResponsiveMenuLinkClick,document.querySelector(".menu-container").onclick=handleResponsiveNav,document.querySelector(".take-tour").addEventListener("click",handleTakeATourClick),document.querySelector(".go-up").addEventListener("click",handleGoUpClick),document.addEventListener("scroll",handleScrollEvents);
+
+
+
+
+
+
+	/*for(let i =0; i<localStorage.length;i++)
+	{
+		const item1=localStorage.key(i);
+		const value = localStorage.getItem(item1);
+
+		//isOutput.innerHTML += `${key}: ${value}<br  />`;
+		var op=isOutput.innerHTML += `${item1}<br  />`;
+		item1.value = op;
+	}*/
+
+	
